3cfab649f8a5fe4e2fc57ab6dde19168239ffe7e
Saad Ahmad <saadahmad8924@gmail.com>
2026-03-01 00:38:20 +0530

    my project 1

--- code.py
+++ code.py
@@ -1,1 +1,47 @@
-a = "saad"
+
+class Chatbook:
+    def __init__(self):
+        self.email = ""
+        self.passwd = ''
+        self.loggedin = False
+        self.menu()
+
+    def menu(self):
+        user_input = input("Welcome to chatbook Please choose an option !" \
+        "1. Signup" \
+        "2. Signin" \
+        "3. Post an article" \
+        "4. Press to exit")
+
+        if user_input == "1":
+            self.signup()
+        elif user_input == "2":
+            self.signin()
+        elif user_input == "3":
+            pass
+        else:
+            exit()
+
+    def signup(self):
+        self.email = input("Please enter your email: ")
+        self.passwd = input("Please enter your password: ")
+        print("You have signedup successfully!!")
+        print("\n")
+        self.menu()   
+
+    def signin(self):
+        if self.email =='' and self.passwd == '':
+            print("Please signup first by pressing 1 then login")
+        else:
+            uname = input("Please enter your username/email: ")
+            pwd = input("Please enter your password: ")    
+            if self.email == uname and self.passwd == pwd:
+                print("You have signedin successfully!")
+                self.loggedin = True
+            else:
+                print("Please input correct creds..")
+        print("\n")
+        self.menu() 
+                            
+
+obj = Chatbook()
